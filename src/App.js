41163732c67caaebbeb541c9baf5cedf308180f4
luciabrumnich <luciabrumnichhr@gmail.com>
2022-05-23 12:11:17 -0300

    Characters grid created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
 import Navbar from "./Components/Navbar";
+import Characters from "./Components/Characters";
 
 function App() {
-  return <Navbar brand={"Rick n Morty"} />;
+  const [characters, setCharacters] = useState([]);
+
+  const initialUrl = "https://rickandmortyapi.com/api/character";
+  function fetchCharacters(url) {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setCharacters(data.results))
+      .catch((error) => console.log(error));
+  }
+  useEffect(() => {
+    fetchCharacters(initialUrl);
+  }, []);
+  return (
+    <>
+      <Navbar brand={"Rick n Morty"} />;
+      <div className="container mt-5">
+        <Characters characters={characters}/>
+      </div>
+    </>
+  );
 }
 
 export default App;
